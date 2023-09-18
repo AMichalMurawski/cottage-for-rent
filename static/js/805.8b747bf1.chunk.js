97ddd198b8b0b3d99901f162b6adf24af62539b8
AMichalMurawski <109108678+AMichalMurawski@users.noreply.github.com>
2023-09-18 20:39:49 +0000

    Deploying to gh-pages from @ AMichalMurawski/domeczki@a9440ec6d4ec0c72bd3c7e3870c4dbeaf3047bba 🚀

---
+++ static/js/805.8b747bf1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcottage_for_rent=self.webpackChunkcottage_for_rent||[]).push([[805],{805:function(e,r,t){t.r(r);var n=t(184);r.default=function(){return(0,n.jsx)("div",{style:{backgroundColor:"lightgray",padding:"30px"},children:(0,n.jsx)("h2",{children:"Nasze domki"})})}}}]);
+//# sourceMappingURL=805.8b747bf1.chunk.js.map
